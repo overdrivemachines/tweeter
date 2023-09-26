cd43aac8592774217d9885e243316ed1534021cb
Dipen Chauhan <get.dipen@gmail.com>
2023-09-26 10:51:38 -0700

    adding the create action

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,13 +2,31 @@ class TweetsController < ApplicationController
   # @route GET /tweets (tweets)
   # @route GET / (root)
   def index
+    @tweet = Tweet.new
+    @tweets = Tweet.order(created_at: :asc)
   end
 
   # @route POST /tweets (tweets)
   def create
+    @tweet = Tweet.new(tweet_params)
+    respond_to do |format|
+      if @tweet.save
+        format.turbo_stream
+      else
+        format.html do
+          flash[:tweet_errors] = @tweet.errors.full_messages
+          redirect_to root_path
+        end
+      end
+    end
   end
 
   # @route DELETE /tweets/:id (tweet)
-  def destroy
+  def destroy; end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:body)
   end
 end
