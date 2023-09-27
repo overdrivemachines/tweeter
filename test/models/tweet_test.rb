f54d078d9bf36ab840c987cfd3ecd39d186cce61
Dipen Chauhan <get.dipen@gmail.com>
2023-09-27 16:44:39 -0700

    annotated

--- test/models/tweet_test.rb
+++ test/models/tweet_test.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: tweets
+#
+#  id         :integer          not null, primary key
+#  body       :text
+#  user_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require "test_helper"
 
 class TweetTest < ActiveSupport::TestCase
