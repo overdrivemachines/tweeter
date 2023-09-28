a15ef2bf24aaad28125981319464012d227b0430
Dipen Chauhan <get.dipen@gmail.com>
2023-09-28 01:45:29 -0700

    attached profile_image

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,23 @@
 # Table name: users
 #
 #  id                     :integer          not null, primary key
+#  admin                  :boolean          default(FALSE)
 #  email                  :string           default(""), not null
 #  encrypted_password     :string           default(""), not null
-#  reset_password_token   :string
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
 #  first_name             :string
 #  last_name              :string
-#  admin                  :boolean          default(FALSE)
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  username               :string
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_username              (username) UNIQUE
 #
 class User < ApplicationRecord
   has_person_name
@@ -25,6 +32,8 @@ class User < ApplicationRecord
   has_many :tweets
 
   validates_uniqueness_of :username
+
+  has_one_attached :profile_image
 
   def full_name
     "#{first_name} #{last_name}"
