322f5b61bff2629e0d5158d001e5236139227116
Dipen Chauhan <get.dipen@gmail.com>
2023-09-27 18:03:57 -0700

    added more users one with same username

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,24 @@
 FactoryBot.define do
   factory :user do
+    username { "user1" }
+    email { Faker::Internet.email }
+    password { "password" }
+    password_confirmation { "password" }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+  end
+
+  factory :user2, class: User do
+    username { "user2" }
+    email { Faker::Internet.email }
+    password { "password" }
+    password_confirmation { "password" }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+  end
+
+  factory :user3, class: User do
+    username { "user1" }
     email { Faker::Internet.email }
     password { "password" }
     password_confirmation { "password" }
