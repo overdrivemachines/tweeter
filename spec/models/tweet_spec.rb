4a99f0d866b3f054282ff9b7bf64de055ae98ce1
Dipen Chauhan <get.dipen@gmail.com>
2023-09-28 01:45:36 -0700

    annotated

--- spec/models/tweet_spec.rb
+++ spec/models/tweet_spec.rb
@@ -1,3 +1,21 @@
+# == Schema Information
+#
+# Table name: tweets
+#
+#  id         :integer          not null, primary key
+#  body       :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :integer          not null
+#
+# Indexes
+#
+#  index_tweets_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  user_id  (user_id => users.id)
+#
 require 'rails_helper'
 
 RSpec.describe Tweet, type: :model do
--- test/models/tweet_test.rb
+++ test/models/tweet_test.rb
@@ -4,9 +4,17 @@
 #
 #  id         :integer          not null, primary key
 #  body       :text
-#  user_id    :integer          not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer          not null
+#
+# Indexes
+#
+#  index_tweets_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  user_id  (user_id => users.id)
 #
 require "test_helper"
 
